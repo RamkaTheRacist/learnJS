e98257405c87ded08ce619b2600ea3844cc14da2
Ramka <whereisourgod@gmail.com>
2023-02-16 16:46:54 +1000

    2.8 done

---
+++ learnJS/2.8/tasks/task1/t1.js
@@ -0,0 +1,10 @@
+/*Каким будет результат этих выражений?
+
+5 > 4                   == true
+"ананас" > "яблоко"     == false
+"2" > "12"              == true // проверка строк посимвольно, т.е. 2 > 1
+undefined == null       == true
+undefined === null      == false
+null == "\n0\n"         == false
+null === +"\n0\n"       == false
+*/
