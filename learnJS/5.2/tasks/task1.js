c3b4c598038803aa6a0ae138e93bda5bf63920a2
Ramka <whereisourgod@gmail.com>
2023-02-17 16:02:32 +1000

    5.2 done

---
+++ learnJS/5.2/tasks/task1.js
@@ -0,0 +1,10 @@
+/*Создайте скрипт, который запрашивает ввод двух чисел (используйте prompt) и после показывает их сумму.
+
+Запустить демо
+
+P.S. Есть «подводный камень» при работе с типами.*/
+
+let firstNumber = prompt("Ur first num?","");
+let secondNumber = prompt("Ur second num?","");
+
+alert(+firstNumber + +secondNumber);
