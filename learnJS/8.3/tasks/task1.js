a72da1ece7fb9f3ba69f41b9f397b58a3bf8fd39
Ramka <whereisourgod@gmail.com>
2023-03-16 10:31:49 +1000

    done

---
+++ learnJS/8.3/tasks/task1.js
@@ -0,0 +1,13 @@
+/*Добавьте всем функциям в прототип метод defer(ms), который вызывает функции через ms миллисекунд.
+
+После этого должен работать такой код:
+
+function f() {
+  alert("Hello!");
+}
+
+f.defer(1000); // выведет "Hello!" через 1 секунду*/
+
+Function.prototype.defer = function (ms) {
+    setTimeout(this, ms);
+};
